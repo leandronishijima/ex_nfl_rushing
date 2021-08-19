a9fdd6796995f090ed3b93eca66e7fcf87b5dcd7
Leandro Nishijima <4566637+leandronishijima@users.noreply.github.com>
2021-08-19 14:52:54 -0300

    fixes column orders + tests

--- test/ex_nfl_rushing_web/live/football_player_rushing_live_test.exs
+++ test/ex_nfl_rushing_web/live/football_player_rushing_live_test.exs
@@ -3,34 +3,68 @@ defmodule NflRushingWeb.FootballPlayerRushingLiveTest do
 
   import Phoenix.LiveViewTest
 
-  alias NflRushing.Statistic
-
-  @create_attrs %{
-    "1st": 42,
-    "1st%": 42,
-    "20+": 42,
-    "40+": 42,
-    att: 42,
-    "att/g": 42,
-    avg: "120.5",
-    fum: 42,
-    lng: "some lng",
-    player: "some player",
-    pos: "some pos",
-    td: 42,
-    team: "some team",
-    yds: 42,
-    "yds/g": 42
-  }
-
-  defp fixture(:football_player_rushing) do
-    {:ok, football_player_rushing} = Statistic.create_football_player_rushing(@create_attrs)
-    football_player_rushing
-  end
+  alias NflRushing.{Repo, Statistic.FootballPlayerRushing}
+  alias NflRushingWeb.FootballPlayerRushingLive.Index
 
   defp create_football_player_rushing(_) do
-    football_player_rushing = fixture(:football_player_rushing)
-    %{football_player_rushing: football_player_rushing}
+    joe_banyard =
+      Repo.insert!(%FootballPlayerRushing{
+        player: "Joe Banyard",
+        team: "JAX",
+        pos: "RB",
+        att: 2,
+        "att/g": 2,
+        yds: 7,
+        avg: 3.5,
+        "yds/g": 7,
+        td: 0,
+        lng: "7",
+        "1st": 0,
+        "1st%": 0,
+        "20+": 0,
+        "40+": 0,
+        fum: 0
+      })
+
+    joe_flacco =
+      Repo.insert!(%FootballPlayerRushing{
+        player: "Joe Flacco",
+        team: "BAL",
+        pos: "QB",
+        att: 2,
+        "att/g": 2,
+        yds: 58,
+        avg: 3.5,
+        "yds/g": 7,
+        td: 2,
+        lng: "16",
+        "1st": 0,
+        "1st%": 0,
+        "20+": 0,
+        "40+": 0,
+        fum: 0
+      })
+
+    shaun_hill =
+      Repo.insert!(%FootballPlayerRushing{
+        player: "Shaun Hill",
+        team: "MIN",
+        pos: "QB",
+        att: 2,
+        "att/g": 2,
+        yds: 5,
+        avg: 3.5,
+        "yds/g": 7,
+        td: 1,
+        lng: "9",
+        "1st": 0,
+        "1st%": 0,
+        "20+": 0,
+        "40+": 0,
+        fum: 0
+      })
+
+    {:ok, statistics: [joe_banyard, joe_flacco, shaun_hill]}
   end
 
   describe "Index" do
@@ -38,13 +72,237 @@ defmodule NflRushingWeb.FootballPlayerRushingLiveTest do
 
     test "lists all football_players_rushings", %{
       conn: conn,
-      football_player_rushing: football_player_rushing
+      statistics: football_players_rushings
     } do
-      {:ok, _index_live, html} =
-        live(conn, Routes.football_player_rushing_index_path(conn, :index))
+      {:ok, _index_live, html} = live(conn, Routes.live_path(conn, Index))
 
       assert html =~ "Listing Football players rushings"
-      assert html =~ football_player_rushing.lng
+
+      Enum.each(
+        football_players_rushings,
+        &assert(html =~ &1.player)
+      )
+    end
+
+    test "lists all football_players_rushings with player filter", %{
+      conn: conn,
+      statistics: [joe_banyard, joe_flacco, shaun_hill]
+    } do
+      {:ok, index_live, html} = live(conn, Routes.live_path(conn, Index))
+
+      html =
+        index_live
+        |> element("form")
+        |> render_change(%{player: "shaun"})
+
+      assert html =~ "shaun"
+      assert html =~ shaun_hill.player
+      refute html =~ joe_banyard.player
+      refute html =~ joe_flacco.player
+    end
+
+    test "lists all football_players_rushings with sort by yds", %{conn: conn} do
+      {:ok, index_live, _html} = live(conn, Routes.live_path(conn, Index))
+
+      assert [
+               {"tbody", [{"id", "football_players_rushings"}],
+                [
+                  {"tr", _,
+                   [
+                     {"td", _, ["Joe Flacco"]},
+                     {"td", _, ["BAL"]},
+                     {"td", _, ["QB"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["58"]} = _yds_flacco,
+                     {"td", _, ["3.5"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["16"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]}
+                   ]},
+                  {"tr", _,
+                   [
+                     {"td", _, ["Joe Banyard"]},
+                     {"td", _, ["JAX"]},
+                     {"td", _, ["RB"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["7"]} = _yds_banyard,
+                     {"td", _, ["3.5"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]}
+                   ]},
+                  {"tr", _,
+                   [
+                     {"td", _, ["Shaun Hill"]},
+                     {"td", _, ["MIN"]},
+                     {"td", _, ["QB"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["5"]} = _yds_hill,
+                     {"td", _, ["3.5"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["1"]},
+                     {"td", _, ["9"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]}
+                   ]}
+                ]}
+             ] =
+               index_live
+               |> element("#yds-sort")
+               |> render_click()
+               |> Floki.find("#football_players_rushings")
+    end
+
+    test "lists all football_players_rushings with sort by td", %{conn: conn} do
+      {:ok, index_live, _html} = live(conn, Routes.live_path(conn, Index))
+
+      assert [
+               {"tbody", [{"id", "football_players_rushings"}],
+                [
+                  {"tr", _,
+                   [
+                     {"td", _, ["Joe Flacco"]},
+                     {"td", _, ["BAL"]},
+                     {"td", _, ["QB"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["58"]},
+                     {"td", _, ["3.5"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["16"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]}
+                   ]},
+                  {"tr", _,
+                   [
+                     {"td", _, ["Shaun Hill"]},
+                     {"td", _, ["MIN"]},
+                     {"td", _, ["QB"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["5"]},
+                     {"td", _, ["3.5"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["1"]},
+                     {"td", _, ["9"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]}
+                   ]},
+                  {"tr", _,
+                   [
+                     {"td", _, ["Joe Banyard"]},
+                     {"td", _, ["JAX"]},
+                     {"td", _, ["RB"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["3.5"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]}
+                   ]}
+                ]}
+             ] =
+               index_live
+               |> element("#td-sort")
+               |> render_click()
+               |> Floki.find("#football_players_rushings")
+    end
+
+    test "lists all football_players_rushings with sort by lng", %{conn: conn} do
+      {:ok, index_live, _html} = live(conn, Routes.live_path(conn, Index))
+
+      assert [
+               {"tbody", [{"id", "football_players_rushings"}],
+                [
+                  {"tr", _,
+                   [
+                     {"td", _, ["Joe Flacco"]},
+                     {"td", _, ["BAL"]},
+                     {"td", _, ["QB"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["58"]},
+                     {"td", _, ["3.5"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["16"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]}
+                   ]},
+                  {"tr", _,
+                   [
+                     {"td", _, ["Shaun Hill"]},
+                     {"td", _, ["MIN"]},
+                     {"td", _, ["QB"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["5"]},
+                     {"td", _, ["3.5"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["1"]},
+                     {"td", _, ["9"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]}
+                   ]},
+                  {"tr", _,
+                   [
+                     {"td", _, ["Joe Banyard"]},
+                     {"td", _, ["JAX"]},
+                     {"td", _, ["RB"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["2"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["3.5"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["7"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]},
+                     {"td", _, ["0"]}
+                   ]}
+                ]}
+             ] =
+               index_live
+               |> element("#lng-sort")
+               |> render_click()
+               |> Floki.find("#football_players_rushings")
     end
   end
 end
